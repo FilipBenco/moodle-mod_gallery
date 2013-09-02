52cc51c4f474a61c49eb833c792dd3c0099f272c
Filip <mail@filipbenco.sk>
2013-09-02 14:59:59 +0200

    intro thumbnails functionality

--- js/intro.js
+++ js/intro.js
@@ -3,7 +3,7 @@ M.mod_gallery = M.mod_gallery || {};
 M.mod_gallery.init = function(Y, cfg) {
     this.Y = Y;
     this.currentPosition = new Array();
-    this.maxScroll = Y.one(".mod-gallery-intro-thumb-cont-helper > ul").get('winWidth')-Y.one('.mod-gallery-intro-thumbnails-container').get('winWidth');    
+    this.maxScroll = Y.one(".mod-gallery-intro-thumb-cont-helper > ul").get('winWidth')-Y.one('.mod-gallery-intro-thumb-cont-helper').get('docWidth');    
 };
 
 M.mod_gallery.moveIntro = function(direction,gallery) {

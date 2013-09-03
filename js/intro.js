2f39fb119e46e9278e12c156369709de7b3c55ed
Filip <mail@filipbenco.sk>
2013-09-03 11:40:32 +0200

    downloading photos functionality

--- js/intro.js
+++ js/intro.js
@@ -1,31 +1,22 @@
 M.mod_gallery = M.mod_gallery || {};
 
 M.mod_gallery.init = function(Y, cfg) {
-    this.Y = Y;
-    this.currentPosition = new Array();
-    this.maxScroll = Y.one(".mod-gallery-intro-thumbnails-table").get('winWidth');    
+    this.Y = Y;  
 };
 
 M.mod_gallery.moveIntro = function(direction,gallery) {
-    if(!(this.currentPosition[gallery]))
-        this.currentPosition[gallery] = 0;
+    var scroll = M.mod_gallery.Y.one('#mod-gallery-intro-thumb-cont-helper-'+gallery).get('docScrollX');
     if(direction === 'left') {
-        if(M.mod_gallery.currentPosition[gallery] < 154)
-            M.mod_gallery.currentPosition[gallery] = 0;
-        else
-            M.mod_gallery.currentPosition[gallery] -= 154;
+       scroll -= 154;
     } else if(direction ==='right') {
-        if((M.mod_gallery.maxScroll - M.mod_gallery.currentPosition[gallery]) < 154)
-            M.mod_gallery.currentPosition[gallery] = M.mod_gallery.maxScroll;
-        else
-            M.mod_gallery.currentPosition[gallery] += 154;
+        scroll += 154;
     }
     
     var move = new M.mod_gallery.Y.Anim({
             node: '#mod-gallery-intro-thumb-cont-helper-'+gallery,
             duration: 0.5,
             to: {
-                scroll: [M.mod_gallery.currentPosition[gallery],0]
+                scroll: [scroll,0]
             }
         });
     move.run();

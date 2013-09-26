4574b60bf2a4e54f9015c63da6ec8cbe10afef14
FilipBenco <b3nnyf@gmail.com>
2013-09-26 12:28:41 +0200

    EN language update

--- lang/en/gallery.php
+++ lang/en/gallery.php
@@ -1,59 +1,97 @@
 <?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://elf.phil.muni.cz/moodledev/moodle2
+ *
+ * @package    mod
+ * @subpackage gallery
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
 defined('MOODLE_INTERNAL') || die;
 
 $string['addimages'] = 'Add Images';
 $string['attachments'] = 'Attachments';
 $string['author'] = 'Author';
-$string['batchrun'] = 'update';
-$string['confirmdelete'] = 'Do you really want to delete selected image?';
-$string['deleteimage'] = 'Delete image';
+$string['batchrun'] = 'Go';
+$string['confirmdelete'] = 'Do you really want to delete selected images?';
 $string['delete'] = 'Delete';
+$string['deleteimage'] = 'Delete image';
 $string['description'] = 'Description:';
 $string['download'] = 'Download';
-$string['downloadphotos'] = 'Download photos';
-$string['drafts'] = 'Drafts';
+$string['downloadphotos'] = 'Download all images';
 $string['edit'] = 'Edit';
 $string['editimage'] = 'Edit image';
 $string['editimages'] = 'Edit images';
+string['gallery:addimages'] = 'Add images to gallery';
 $string['gallery:addinstance'] = 'Add new Gallery instance';
-$string['gallery:view'] = 'View Gallery instance';
-$string['gallery:manage'] = 'Edit Gallery instance';
-$string['gallery:addimages'] = 'Add images in gallery';
-$string['gallery:editallimages'] = 'Edit each image in gallery';
-$string['gallery:editownimages'] = 'Edit own images in gallery';
-$string['gallery:deleteallimages'] = 'Delete each image in gallery';
-$string['gallery:deleteownimages'] = 'Delete own images in gallery';
+$string['gallery:deleteallimages'] = 'Delete all images';
+$string['gallery:deleteownimages'] = 'Delete your own images';
+$string['gallery:editallimages'] = 'Edit all images';
+$string['gallery:editownimages'] = 'Edit your own images';
+$string['gallery:manage'] = 'Manage gallery';
+$string['gallery:view'] = 'View gallery content';
 $string['galleryname'] = 'Name:';
-$string['imageattachments'] = 'Image attachments';
-$string['imageattachments_help'] = 'Image attachments help';
-$string['imagename'] = 'Image name:';
+$string['imageattachments'] = 'Allow image attachments';
+$string['imageattachments_help'] = 'If enabled, each image in your gallery can have files attach to it, which will only show with the particular image within the gallery preview mode. In this way, you can enhance the content of your gallery by texts, sounds, videos, and other files relevant for each image.';
+$string['imagename'] = 'Image name';
 $string['images'] = 'Images';
-$string['missingsourceerror'] = 'You need to specifiy an image source';
+$string['missingsourceerror'] = 'You need to specify the source of this image.';
 $string['modulename'] = 'Gallery';
-$string['modulename_help'] = 'Module help';
+$string['modulename_help'] = 'The Gallery resource module enables organizing, managing and viewing a gallery of images within your course.
+
+As a teacher, you are able to create, edit and delete galleries. For all images, small thumbnails are automatically generated, which are used for the thumbnail view of the gallery. Clicking on any of the thumbnails brings you to the presentation mode where you can browse through the images in large resolution and full-screen view (after clicking on a selected image).
+
+For any image, users are able to leave comments on your gallery. If enabled, you can also add file attachments to each of the images, enhancing the content of your gallery by text and multimedia files.
+
+If needed, the gallery module offers a specific set of permissions to allow students add their own images without being able to manage the gallery module itself. In this way, you can create a shared course image collection, run student image competitions etc.
+
+A gallery may be used
+<ul>
+<li>To display a collection of image resources</li>
+<li>To display a collection of pictures relevant for a given topic</li>
+<li>To collect students\' own photos</li>
+</ul>';
+
 $string['modulenameplural'] = 'Galleries';
-$string['moveimage'] = 'move image';
-$string['nextimage'] = 'Next Image';
+$string['moveimage'] = 'Move image';
 $string['next'] = 'Next';
-$string['nopermission'] = 'Sorry, but you don\'t have a permission to do this action.';
+$string['nextimage'] = 'Next Image';
+$string['nopermission'] = 'Sorry, but you do not have permissions to perform this action.';
 $string['pluginadministration'] = 'Gallery administration';
 $string['pluginname'] = 'Gallery';
 $string['previews'] = 'Previews';
-$string['previousimage'] = 'Previous Image';
 $string['previous'] = 'Previous';
-$string['returntogallery'] = 'Return to gallery';
-$string['rotateleft'] = 'Left rotate';
-$string['rotateright'] = 'Right rotate';
+$string['previousimage'] = 'Previous Image';
+$string['returntogallery'] = 'Back to image thumbnails';
+$string['rotateleft'] = 'Rotate left';
+$string['rotateright'] = 'Rotate right';
 $string['savechanges'] = 'Save changes';
 $string['saveimages'] = 'Save images';
-$string['selectdeselectall'] = 'Select/Deselect all';
-$string['selectedimageslabel'] = 'Selected images:';
-$string['showthumbnails'] = 'Show intro thumbnails';
-$string['showthumbnails_help'] = 'Show intro thumbnails help';
+$string['selectdeselectall'] = 'Select all / Deselect all';
+$string['selectedimageslabel'] = '<strong>With selected:</strong>';
+$string['showthumbnails'] = 'Display image thumbnails on course page';
+$string['showthumbnails'] = 'Display image thumbnails on course page';
+$string['showthumbnails_help'] = 'If enabled, interactive thumbnails of all gallery images will be displayed directly on the course page.';
 $string['source'] = 'Source';
-$string['sourceown'] = 'My picture';
+$string['sourceown'] = 'My own picture';
 $string['thumbnails'] = 'Thumbnails';
-$string['turneditingoff'] = 'End editing gallery';
-$string['turneditingon'] = 'Start editing gallery';
-$string['uploadimages'] = 'Upload Images';
-$string['viewpreview'] = 'View preview';
+$string['turneditingoff'] = 'Turn image editing off';
+$string['turneditingon'] = 'Turn image editing on';
+$string['uploadimages'] = 'Upload images';
+$string['viewpreview'] = 'View image presentation';

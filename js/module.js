b8e8a3aef0ac6faedd42bb547691ee110beb25f4
FilipBenco <b3nnyf@gmail.com>
2013-10-21 11:05:56 +0200

    Fix - If disabled image download

--- js/module.js
+++ js/module.js
@@ -4,7 +4,7 @@ M.mod_gallery.init = function(Y, cfg) {
     this.Y = Y;
     this.context = cfg.context;
     this.currentImage = cfg.currentImage;
-    this.showoriginal = cfg.showoriginal;
+    this.showOriginal = cfg.showOriginal;
     this.perm = new Array();
     this.perm[cfg.currentImage] = new Array();
     this.perm[cfg.currentImage][0] = cfg.canEdit;
@@ -14,7 +14,7 @@ M.mod_gallery.init = function(Y, cfg) {
 M.mod_gallery.showImage = function(imageId) {
     M.mod_gallery.Y.one('#mod-gallery-image-perview-a-'+M.mod_gallery.currentImage).hide();
     M.mod_gallery.Y.one('#mod-gallery-image-perview-a-'+imageId).show();
-   if(M.mod_gallery.showoriginal)
+   if(M.mod_gallery.showOriginal)
        M.mod_gallery.Y.one('#mod-gallery-image-preview-download > a').setAttribute('href',M.mod_gallery.Y.one('#mod-gallery-image-perview-a-'+imageId).getAttribute('href'));
     M.mod_gallery.Y.one('#mod-gallery-image-perview-a-'+imageId)
     if(M.mod_gallery.Y.one('#mod-gallery-image-desc-' + imageId).get('innerHTML') !== '' ||
